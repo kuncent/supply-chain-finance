912ba77c2565a39269f8cbefa18c0ad04222d3f3
蔡志恒 <caizh@jpush.cn>
2020-06-29 17:18:59 +0800

    update

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://113.31.131.13:8086/admin"'
+})
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://113.31.131.13:8086/admin"'
+}
